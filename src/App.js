b1e14227f1a15366f0d7f56ea22be75f7df11ad5
cupaxx <fiqri.fa42@gmail.com>
2021-06-04 19:50:11 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/button";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    count: 0,
+    error: ''
+  }
+  componentDidUpdate(){
+    console.log(this.state)
+  }
+
+  plus = () => {
+    if(this.state.count >= 10){
+      this.setState({error: 'Cant increase greater than 10'})
+    }else {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }
+    
+  };
+
+  minus= () => {
+    if(this.state.count <= 0){
+      this.setState({error: 'Cant decrease greater than 0'})
+    }else {
+      this.setState({
+        count: this.state.count - 1
+      })
+    }
+  };
+
+
+
+  render() {
+    let { count } = this.state;
+    return (
+      <div className="app">
+        <div>
+          <div className="count">
+            <h3>Count</h3>
+          </div>
+          <div className="section1">
+          <div className="buttons">
+            <Button title={"-"} click={this.minus} />
+          </div>
+          <div className="textCount">
+          <h1>{count}</h1>
+          </div>
+          <div className="buttons">
+            <Button title={"+"} click={this.plus} />
+          </div>
+          </div>
+          <div className="errorM">{this.state.error!==''&&this.state.error}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
